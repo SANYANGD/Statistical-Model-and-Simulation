fe4a687ed34364e26a01b48bdc177e8c7bdcdcd1
SANYANGD <chunyangdai@outlook.com>
2020-09-09 15:41:04 +0800

    hw1

--- demo/test.R
+++ demo/test.R
@@ -3,3 +3,8 @@
 # Created by: chunyang
 # Created on: 9/9/2020
 
+f = c(0,1)
+for(i in 3:20){
+  f[i] = f[i-2] + f[i-1]
+}
+print(f)
---
+++ homework/homework01.R
@@ -0,0 +1,13 @@
+# Title     : TODO
+# Objective : TODO
+# Created by:
+# Created on: 9/9/2020
+
+seq1 = rep(0:4, each = 5) # 0 0 0 0 0 1 1 1 1 1 2 2 2 2 2 3 3 3 3 3 4 4 4 4 4
+print(seq1)
+
+seq2 = rep(1:5, 5) # 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5
+print(seq2)
+
+seq3 = seq1 + seq2 # 1 2 3 4 5 2 3 4 5 6 3 4 5 6 7 4 5 6 7 8 5 6 7 8 9
+print(seq3)
