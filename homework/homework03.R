c030c636653c61bc170bd6736cac0b6338331e87
SANYANG <chunyangdai@outlook.com>
2020-09-23 10:03:01 +0800

    hm3

--- homework/homework03.R
+++ homework/homework03.R
@@ -1,6 +1,25 @@
 # Title     : TODO
 # Objective : TODO
-# Created by: SCDX
+# Created by: 代春洋2018141493004
 # Created on: 2020/9/23
 
-sample(1:100,10,replace=F,prob=rep(1/10,10))
+chouqv = function(n) {
+  s = 0
+  chou = sample(1:n, n, replace = F, prob = rep(1 / n, n))
+  for (i in 1:n) {
+    if (i == chou[i]) {
+      s = s + 1
+    }
+  }
+  return (s)
+}
+
+a = c()
+for (i in 1:10000){
+  a[i] = chouqv(100)
+}
+mean_a = mean(a)
+mean_a #期望
+var_a = var(a)
+var_a #方差
+
