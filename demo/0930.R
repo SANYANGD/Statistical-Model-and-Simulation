9e9bb0403635b4312804ffdfee077c321a9dc60f
SCDX <chunyangdai@outlook.com>
2020-09-30 10:28:36 +0800

    0930

--- demo/0930.R
+++ demo/0930.R
@@ -30,3 +30,15 @@ par(new = T)
 curve(1 - exp(-0.5 * x), 0, 10, ylim = c(0, 1))
 par(new = T)
 hist(-2 * log(runif(100000)), freq = F, xlim = c(0, 10), ylim = c(0, 1), breaks = 100)
+
+
+#
+u1 = runif(100000)
+u2 = runif(100000)
+k = 2 * sqrt(pi)
+y = (-3 / 2) * log(u1)
+c = (3^1.5) / (2 * pi * exp(1))^0.5
+accept = y[u2 <= k * ((y^0.5) * exp(-y) / c * (2 / 3) * exp(-2 * y / 3))]
+hist(accept, freq = F, xlim = c(0, 10), breaks = seq(0, 100, 0.02))
+par(new = T)
+plot(10 * u1, ((10 * u1)^0.5) * exp(-u1 * 10) * 2 / sqrt(pi))
