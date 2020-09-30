6ba17ef879e2cf642684b242985e1014024b1f33
SCDX <chunyangdai@outlook.com>
2020-09-30 09:22:31 +0800

    0930

--- demo/0930.R
+++ demo/0930.R
@@ -20,4 +20,13 @@ array = c()
 for (i in 1:10000) {
   array[i] = select()
 }
-table(array)/10000
+table(array) / 10000
+
+
+# 指数分布抽样
+# lamda=0.5
+curve(0.5 * exp(-0.5 * x), 0, 10, ylim = c(0, 1), col = 'green')
+par(new = T)
+curve(1 - exp(-0.5 * x), 0, 10, ylim = c(0, 1))
+par(new = T)
+hist(-2 * log(runif(100000)), freq = F, xlim = c(0, 10), ylim = c(0, 1), breaks = 100)
