2958dd54021af5bcdf10a1ee4fd898f46b5098a8
SCDX <chunyangdai@outlook.com>
2020-09-30 09:58:13 +0800

    hw05

---
+++ homework/homework05.R
@@ -0,0 +1,13 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: SCDX
+# Created on: 2020/9/30
+
+# f(x) = (e*x) / (e-1) , 0 =< x =<1
+hist(log((exp(1)-1)*runif(100000)+1), freq = F, xlim = c(0, 1), ylim=c(0,1.6), breaks = 50)
+par(new = T)
+curve((exp(x) - 1) / (exp(1) - 1), xlim = c(0, 1), ylim=c(0,1.6), col = "red")
+par(new = T)
+curve(exp(x) / (exp(1) - 1), ylim=c(0,1.6), col = "green")
+
+
