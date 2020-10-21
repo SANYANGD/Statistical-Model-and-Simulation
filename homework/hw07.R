80f485a7742065d65af287f9d70acf97e33ea0fb
SANAYNG <chunyangdai@outlook.com>
2020-10-21 10:49:14 +0800

    hw07

--- homework/hw07.R
+++ homework/hw07.R
@@ -4,35 +4,20 @@
 # Created on: 2020/10/21
 
 #泊松分布抽样
-countcost = function() {
-  cost = c()
-  for (i in c(1, 2000)) {
-    sum = 0
-    for (t in c(1, 12)) {
-      n = rpois(1, 5)
-      money = runif(n, min = 2000, max = 10000)
-      sum = sum + sum(money)
-    }
-    mean = sum
-    all = c(cost, mean)
-  }
-  cost = mean(all) * 0.99
-  return(cost)
-}
 
-huifei = c()
+cost = 0
 all = c()
-#遍历2000名用户
-for (i in 1:2000)
-{
-  #一个用户一年需要的金钱
+for (i in 1:10000) {
   sum = 0
-  for (t in 1:12)
-  {
-    cishu = rpois(1, 5)
-    money = runif(cishu, min = 2000, max = 10000)
+  for (j in 1:12) {
+    n = rpois(1, 5)
+    money = runif(n, min = 2000, max = 10000)
     sum = sum + sum(money)
   }
   all = c(all, sum)
 }
-huifei = mean(all) * 0.99
+cost = sort(all)[9901] / 10000
+cost
+
+
+
