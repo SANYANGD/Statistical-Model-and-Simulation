1df162cc6bd99a70b687e76cdd046bdf07ccb041
SANYANG <chunyangdai@outlook.com>
2020-12-02 09:56:54 +0800

    hw1202

--- homework/hw1202.R
+++ homework/hw1202.R
@@ -3,3 +3,23 @@
 # Created by: 代春洋
 # Created on: 2020/12/2
 
+
+#需要先对数据进行排序
+
+data <- sort(c(71, 198, 139, 145, 21, 31, 47, 122, 146, 109))
+
+F <- function(x) {
+  return(1 - exp(-x / 100))
+}
+
+D <- function(data, n) {
+  t <- c()
+  for (j in 1:n) {
+    t <- c(temp, j / n - (F(data[j])))
+    t <- c(temp, (F(data[j])) - (j - 1) / n)
+  }
+  return(max(temp))
+
+}
+
+D(data, 10)
