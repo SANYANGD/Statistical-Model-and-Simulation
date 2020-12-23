2c7441ed85a6afadda2337043c357557a8378ff0
SANYANGD <chunyangdai@outlook.com>
2020-12-23 10:27:11 +0800

    hw1223

--- homework/hw1223.R
+++ homework/hw1223.R
@@ -1,28 +1,32 @@
 # Created by: 代春洋
 # Created on: 2020/12/23
 
-fx1x2 <- function(u1, u2, e1, e2, p, x2) {
+p <- 0.5
+
+f_1_2 <- function(u1, u2, e1, e2, x2) {
   return(rnorm(1, mean = (u1 + p * (e1 / e2) * (x2 - u2)), (1 - p * p) * e1 * e1))
 }
 
-fx2x1 <- function(u1, u2, e1, e2, p, x1) {
+f_2_1 <- function(u1, u2, e1, e2, x1) {
   return(rnorm(1, mean = (u2 + p * (e2 / e1) * (x1 - u1)), (1 - p * p) * e2 * e2))
 }
 
-gibbs <- function(u1, u2, e1, e2, p) {
-  num <- 5000
-  x1List <- vector(length = num)
-  x2List <- vector(length = num)
+G <- function(u1, u2, e1, e2) {
+  n <- 5000
+  list1 <- vector(length = n)
+  list2 <- vector(length = n)
   x2 <- u2
-  for (i in 1:num) {
-    x1 <- fx1x2(u1, u2, e1, e2, p, x2)
-    x2 <- fx2x1(u1, u2, e1, e2, p, x1)
-    x1List[i] <- x1
-    x2List[i] <- x2
+  for (i in 1:n) {
+    x1 <- f_1_2(u1, u2, e1, e2, x2)
+    x2 <- f_2_1(u1, u2, e1, e2, x1)
+    list1[i] <- x1
+    list2[i] <- x2
   }
-  return(list(one = x1List, two = x2List))
+  return(list(a = list1, b = list2))
 }
 
-x_res <- gibbs(0, 2, 1, 0.5, 0.75)$one
-y_res <- gibbs(0, 2, 1, 0.5, 0.75)$two
+x_r <- G(0, 2, 1, 0.5)$a
+x_r
+y_r <- G(0, 2, 1, 0.5)$b
+y_r
 
